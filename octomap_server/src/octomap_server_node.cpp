af197886c492f6a52bcda9e9c4dc5a67f64afb19
johnd2010 <johnd2010>
2023-08-26 00:09:25 +0100

    Adding Rate

--- octomap_server/src/octomap_server_node.cpp
+++ octomap_server/src/octomap_server_node.cpp
@@ -58,25 +58,6 @@ int main(int argc, char** argv){
   OctomapServer server(private_nh, nh);
   ros::spinOnce();
 
-  if (argc == 2){
-    mapFilename = std::string(argv[1]);
-  }
-
-  if (private_nh.getParam("map_file", mapFilenameParam)) {
-    if (mapFilename != "") {
-      ROS_WARN("map_file is specified by the argument '%s' and rosparam '%s'. now loads '%s'", mapFilename.c_str(), mapFilenameParam.c_str(), mapFilename.c_str());
-    } else {
-      mapFilename = mapFilenameParam;
-    }
-  }
-
-  // if (mapFilename != "") {
-  //   if (!server.openFile(mapFilename)){
-  //     ROS_ERROR("Could not open file %s", mapFilename.c_str());
-  //     exit(1);
-  //   }
-  // }
-
   try{
     ros::spin();
   }catch(std::runtime_error& e){
